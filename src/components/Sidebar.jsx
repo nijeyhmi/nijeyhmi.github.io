742d2ff80b4597dfbd67c60cf914455491174650
nijeyhmi <nijeyhmi@gmail.com>
2025-02-11 21:10:28 +0900

    design: Sidebar 디자인 적용용

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,17 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-64 h-screen py-4">
+      <ul className="pl-10">
+        <li className="py-2 hover:font-bold hover:cursor-pointer">Home</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">회고</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">React</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">Vue</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">JS</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">TS</li>
+        <li className="py-2 hover:font-bold hover:cursor-pointer">Etc</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
