3b6f0c5f21c0de93637046b6d368ceb79de36c5f
nijeyhmi <nijeyhmi@gmail.com>
2025-02-12 22:44:19 +0900

    design: Footer 디자인 적용

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,24 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <div className="bg-gray-100 py-6 mt-10">
+      <div className="container mx-auto flex flex-col items-center">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} nijeyhmi's Blog. All rights
+          reserved.
+        </p>
+        <button
+          onClick={scrollToTop}
+          className="absolute right-4 bottom-[-6rem] w-10 h-10 border text-slate-300 rounded-full shadow-md flex items-center justify-center focus:outline-none"
+        >
+          ⬆
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
