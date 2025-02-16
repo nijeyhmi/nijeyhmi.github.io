f10185c25b58e20ca39ec95aeb790c405ab9cd00
nijeyhmi <nijeyhmi@gmail.com>
2025-02-16 16:49:32 +0900

    feat: react router 추가가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
+import PostList from "./pages/PostList";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="flex">
+          <Sidebar />
+          <Main />
+        </div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/category/:categoryName" element={<PostList />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="w-full h-[44px] flex justify-between items-center px-10">
-      <div>nijeyhmi</div>
+      <div>
+        <Link to={`/`} className="text-gray-700 hover:text-black">
+          nijeyhmi
+        </Link>
+      </div>
       <div className="flex">
         <div className="pr-2 border-r">portfolio</div>
         <div className="pl-2">github</div>
