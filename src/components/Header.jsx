a350b2a1682177c4615931113cc2655d10b75e5c
wattk <watt.kwon@gmail.com>
2025-03-10 16:47:11 +0900

    feat: react-icons 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
-const Header = () => {
+import { RxHamburgerMenu } from "react-icons/rx";
+import { CiLight } from "react-icons/ci";
+import { HiHashtag } from "react-icons/hi2";
+import { IoIosSearch } from "react-icons/io";
+const Header = ({ toggleSidebar }) => {
   return (
-    <div className="w-full h-[44px] flex justify-between items-center px-10">
-      <div>
-        <Link to={`/`} className="text-gray-700 hover:text-black">
-          <h2 className="text-lg font-bold">nijeyhmi</h2>
+    <div className="w-full h-16 flex justify-between items-center px-10 shadow-sm shadow-b fixed top-0 right-0 z-10">
+      <div className="flex items-center">
+        <RxHamburgerMenu className="w-6 h-6 mr-4 hover:cursor-pointer" onClick={toggleSidebar} />
+        <Link to={`/`} className="hover:text-gray-800">
+          <h2 className="text-xl font-bold">HYELOG</h2>
         </Link>
       </div>
-      <div className="flex">
-        <div className="pr-2 border-r">portfolio</div>
-        <div className="pl-2">github</div>
+      <div className="flex items-center">
+        <CiLight className="w-7 h-7 mr-2 hover:cursor-pointer" />
+        <HiHashtag className="w-6 h-6 mr-2 hover:cursor-pointer" />
+        <IoIosSearch className="w-7 h-7  hover:cursor-pointer" />
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
-const Sidebar = () => {
-  const categories = ["회고", "React", "Vue", "JS & TS", "도서", "기타"];
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+
+const Sidebar = ({ isOpen }) => {
+  const categories = ["전체", "React", "Vue", "JS & TS", "회고"];
+
   return (
-    <div className="w-64 h-screen py-4 overflow-y-auto">
-      <ul className="pl-10">
+    <div
+      className={`w-64 bg-white shadow-md p-5 h-svh mt-16 fixed top-0 left-0 transition-transform duration-300 ease-in-out flex flex-col ${
+        isOpen ? "translate-x-0" : "-translate-x-64"
+      }`}
+    >
+      <ul className="pl-5 flex-grow">
         {categories.map((category) => (
           <li key={category} className="py-2 hover:font-bold hover:cursor-pointer">
             <Link to={`/${category}`} className="text-gray-700 hover:text-black">
@@ -12,6 +20,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <div className="flex justify-start space-x-4 pl-5 mb-28 border-t pt-5">
+        <MdOutlineMail className="w-6 h-6 mr-2 hover:cursor-pointer" />
+        <FaGithub className="w-6 h-6 mr-2 hover:cursor-pointer" />
+      </div>
     </div>
   );
 };
